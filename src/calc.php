afb4b00603a6db1e1cd643fffb51c7a6556d453f
Zentai Pál <zentaipali@gmail.com>
2022-05-30 08:52:57 +0200

    Számolás kész

--- src/calc.php
+++ src/calc.php
@@ -1,1 +1,30 @@
 <?php
+
+echo file_get_contents('templates/head.html');
+
+
+$page = file_get_contents('templates/calc.html');
+
+function calcVolume($aside, $bside, $cside) {
+    $volume = 4/3 * pi() * $aside * $bside * $cside;
+    return $volume;
+}
+
+if(
+    !empty($_GET['aside']) and 
+    !empty($_GET['bside']) and 
+    !empty($_GET['cside'])
+) {
+    $aside = $_GET['aside'];
+    $bside = $_GET['bside'];
+    $cside = $_GET['cside'];
+    $volume = calcVolume($aside, $bside, $cside);    
+}else {
+    $volume = "Hiba! Helytelen bemenő adatok";
+}
+
+$page = str_replace('{{ result }}', $volume, $page);
+
+echo $page;
+
+echo file_get_contents('templates/foot.html');

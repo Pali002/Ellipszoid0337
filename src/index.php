e9f8c5915ca0e5f49efd9e5a2b84e5b40316becd
Zentai Pál <zentaipali@gmail.com>
2022-05-30 09:02:46 +0200

    Javítás, kész feladat

--- src/index.php
+++ src/index.php
@@ -1,3 +1,13 @@
+<!--
+File: index.php
+Author: Zentai Pál
+Copyright: 2022, Zentai Pál
+Group: Szoft-I-N
+Date: 2022-05-30
+Github: https://github.com/Pali002/
+Licenc: GNU GPL
+-->
+
 <?php
 
 echo file_get_contents('templates/head.html');
